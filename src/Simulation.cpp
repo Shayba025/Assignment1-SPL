cf42fddc3d6880df685ae6843cc58bc9e0afef44
amitbarel8 <amitbarel8610@gmail.com>
2024-12-05 16:25:59 +0200

    Update Simulation.cpp
    finishing all the functions that can run without direct using of actions, of course someof them need modyfications as soon as we finish actions

--- src/Simulation.cpp
+++ src/Simulation.cpp
@@ -1,6 +1,10 @@
 #include "Simulation.h"
 #include <fstream> 
 #include "Auxiliary.h"
+#include "Settlement.h"
+#include "Facility.h"
+#include "SelectionPolicy.h"
+#include "Plan.h"
 
 Simulation::Simulation(const string &configFilePath)
   : isRunning(false),
@@ -10,42 +14,97 @@ Simulation::Simulation(const string &configFilePath)
     settlements(),
     facilitiesOptions()
 {
+    // reading the config file
     std::ifstream configfile(configFilePath);
-    std::string line;
+    string line;
     while (std::getline(configfile, line)) {
-        std::vector<std::string> arguments = Auxiliary::parseArguments(line);
+        // spliting the text into lines
+        vector<string> arguments = Auxiliary::parseArguments(line);
+
+        // the arguments[0] is the type of the object we want to create
+
+        // creating settlement
         if(arguments[0] == "settlement")
         {
+            // we need to modify the text into the enum for settlement variable
             SettlementType typeAsInt = static_cast<SettlementType>(std::stoi(arguments[2]));
+
+            // in the method of simmulation we create the object on the heap and only reatrun to the vctor it's pointer
             Settlement *newSettlement = new Settlement(arguments[1],typeAsInt);
             addSettlement(newSettlement);
         }
             
         else if(arguments[0] == "facility"){
+            // we create a new facilityOption
+            // we declate new facility while converting the text variables to the type the constructor ueses
             FacilityType *newFacility = new FacilityType(arguments[1], static_cast<FacilityCategory>(std::stoi(arguments[2])) ,std::stoi(arguments[3]),std::stoi(arguments[4]),std::stoi(arguments[5]),std::stoi(arguments[6]));
             addFacility(*newFacility);
+            // we're not adding a pointer but the object itself so we delete the object we created to avoide memory leak
             delete(newFacility);
 
         }
-        // else if(arguments[0] == "plan"){
-        //  addPlan();
-        // }
+        else if(arguments[0] == "plan"){
+            // creating the plan based on the selection policy
+            // if the settelement doensn't exists we can't do anything
+            if(isSettlementExists(arguments[1])){
+                SelectionPolicy *selectedPolicy;
+                if(arguments[2] == "eco"){
+                    selectedPolicy = new EconomySelection();
+                    addPlan(getSettlement(arguments[1]), selectedPolicy);
+                    // we're not adding a pointer but the object itself so we delete the object we created to avoide memory leak
+                    delete(selectedPolicy);
+
+                }
+                else if(arguments[2] == "bal"){
+                    // since we creating new plan it's scores are by default 0 (we ahven't build anything)
+                    selectedPolicy = new BalancedSelection(0,0,0);
+                    addPlan(getSettlement(arguments[1]), selectedPolicy);
+                    // we're not adding a pointer but the object itself so we delete the object we created to avoide memory leak
+                    delete(selectedPolicy);
+                    
+                }
+                else if(arguments[2] == "env"){
+                    selectedPolicy = new SustainabilitySelection();
+                    addPlan(getSettlement(arguments[1]), selectedPolicy);
+                    // we're not adding a pointer but the object itself so we delete the object we created to avoide memory leak
+                    delete(selectedPolicy);
+
+                }
+                else if(arguments[2] == "nve"){
+                    selectedPolicy = new NaiveSelection();
+                    addPlan(getSettlement(arguments[1]), selectedPolicy);
+                    // we're not adding a pointer but the object itself so we delete the object we created to avoide memory leak
+                    delete(selectedPolicy);
+            }
+            
+         }
             
         }
 
+    
+}
     configfile.close();
 }
 
-// to do
-void Simulation::start(){
 
+void Simulation::start(){
+    // first we check the simulation is not running
+    if(this->isRunning){
+        std::count << "the Simulation alreadt running you need to clsoe it brfore starting again"
+    }
+    else{
+
+    }
 }
+
 
 void Simulation::addPlan(const Settlement &settlement, SelectionPolicy *selectionPolicy){
     // create and insert
     this->plans.push_back(*new Plan(this->planCounter, settlement, selectionPolicy, this->facilitiesOptions));
     // making changes after init
     this->planCounter = this->planCounter + 1;
+    
+    // delete(selectionPolicy);
 }
 
 bool Simulation::addSettlement(Settlement *settlement){
@@ -60,10 +119,10 @@ bool Simulation::addSettlement(Settlement *settlement){
 
 bool Simulation::addFacility(FacilityType facility){
     // checking if facility name already existing
-    for(auto iter = this->facilitiesOptions.begin() ; iter!= this->facilitiesOptions.end();)
+    for(FacilityType currFacility : this->facilitiesOptions)
     {
-        FacilityType currFacility = *iter;
         if(facility.getName() == currFacility.getName()){
+            // facility already existing
             return false;
         }
     }
@@ -75,9 +134,8 @@ bool Simulation::addFacility(FacilityType facility){
 // we check if the settlement name is in our settlements vector
 // if it is the settlement exist else its not so we return false;
 bool Simulation::isSettlementExists(const string &settlementName){
-    for(auto iter = this->settlements.begin() ; iter!= this->settlements.end();)
+    for(Settlement *currSettlement : this->settlements)
     {
-        Settlement *currSettlement = *iter;
         if(settlementName == currSettlement->getName()){
             return true;
         }
@@ -88,9 +146,8 @@ bool Simulation::isSettlementExists(const string &settlementName){
 
 Settlement &Simulation::getSettlement(const string &settlementName)
 {
-    for(auto iter = this->settlements.begin() ; iter!= this->settlements.end();)
+    for(Settlement *currSettlement : this->settlements)
     {
-        Settlement *currSettlement = *iter;
         if(settlementName == currSettlement->getName()){
             return *currSettlement;
         }
@@ -100,3 +157,15 @@ Settlement &Simulation::getSettlement(const string &settlementName)
     return nullSttlement;
 }
 
+
+void Simulation::to_string(){
+    for(Settlement *set : this->settlements){
+        std::cout << set->toString() << std::endl;
+    }
+    for(FacilityType fac : this->facilitiesOptions){
+        std::cout << fac.toString() << std::endl;
+    }
+    for(Plan pla : this->plans){
+        std::cout << pla.toString() << std::endl;
+    }
+}
