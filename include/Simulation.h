7e34105e77263e32ae89c6c758a764c6551ff0c3
amitbarel8 <amitbarel8610@gmail.com>
2024-12-06 20:48:20 +0200

    Update Simulation.h

--- include/Simulation.h
+++ include/Simulation.h
@@ -1,59 +1,57 @@
 #pragma once
+#include <string>
 #include <vector>
 #include "Facility.h"
+#include "Plan.h"
+#include "Settlement.h"
+using std::string;
 using std::vector;
 
-class SelectionPolicy {
-    public:
-        virtual const FacilityType& selectFacility(const vector<FacilityType>& facilitiesOptions) = 0;
-        virtual const string toString() const = 0;
-        virtual SelectionPolicy* clone() const = 0;
-        virtual ~SelectionPolicy() = default;
-};
+class BaseAction;
+class SelectionPolicy;
 
-class NaiveSelection: public SelectionPolicy {
+class Simulation {
     public:
-        NaiveSelection();
-        const FacilityType& selectFacility(const vector<FacilityType>& facilitiesOptions) override;
-        const string toString() const override;
-        NaiveSelection *clone() const override;
-        ~NaiveSelection() override = default;
+        Simulation(const string &configFilePath); // e
+        /////////////////////////
+        //rule of 5:
+        // Copy Constructor
+       // Simulation::Simulation(const Simulation& other);
+        // Move Constructor
+       // Simulation::Simulation(Simulation&& other) noexcept
+        // check if we need to add rule of 5 opertors
+        ~Simulation();
+        // while contructing and extracting the data from the config file we need to deteminate if we're going to build a plan, settlemet or facility and act accordinly
+        // this function will check the "type" of the line object and create the right object in our code
+        void createObjectBasedType(vector<string> &arguments);
+        // the policy we choose is different based on the commnad line in the config file
+        // this function determinate whatt policy to choose for the plan we're building
+        void createPlanBasedPolicy(vector<string> &arguments);
+        /////////////////////////
+        void start();
+        void addPlan(const Settlement &settlement, SelectionPolicy *selectionPolicy); 
+        void addAction(BaseAction *action);
+        bool addSettlement(Settlement *settlement); // yes
+        bool addFacility(FacilityType facility); // yes
+        bool isSettlementExists(const string &settlementName); // yes
+        Settlement &getSettlement(const string &settlementName); // yes
+        Plan &getPlan(const int planID); // yes
+        void step();
+        void close();
+        void open(); //yes
+
+        void to_string();
+        /////////////////
+        // special functions to interact with the actions:
+        // cehcking if the command meets the critiria, has the correct name and gives all the right arguments
+        void validateCommnad(vector<string> &commandAsvector);
+        void executeStepCommand(int &numOfSteps);
+        /////////////////
     private:
-        int lastSelectedIndex;
-};
-
-class BalancedSelection: public SelectionPolicy {
-    public:
-        BalancedSelection(int LifeQualityScore, int EconomyScore, int EnvironmentScore);
-        const FacilityType& selectFacility(const vector<FacilityType>& facilitiesOptions) override;
-        const string toString() const override;
-        BalancedSelection *clone() const override;
-        ~BalancedSelection() override = default;
-    private:
-        int LifeQualityScore;
-        int EconomyScore;
-        int EnvironmentScore;
-};
-
-class EconomySelection: public SelectionPolicy {
-    public:
-        EconomySelection();
-        const FacilityType& selectFacility(const vector<FacilityType>& facilitiesOptions) override;
-        const string toString() const override;
-        EconomySelection *clone() const override;
-        ~EconomySelection() override = default;
-    private:
-        int lastSelectedIndex;
-
-};
-
-class SustainabilitySelection: public SelectionPolicy {
-    public:
-        SustainabilitySelection();
-        const FacilityType& selectFacility(const vector<FacilityType>& facilitiesOptions) override;
-        const string toString() const override;
-        SustainabilitySelection *clone() const override;
-        ~SustainabilitySelection() override = default;
-    private:
-        int lastSelectedIndex;
+        bool isRunning;
+        int planCounter; //For assigning unique plan IDs
+        vector<BaseAction*> actionsLog;
+        vector<Plan> plans;
+        vector<Settlement*> settlements;
+        vector<FacilityType> facilitiesOptions;
 };
